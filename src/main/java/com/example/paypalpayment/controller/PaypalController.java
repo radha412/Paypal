00fe47ec87c1581c810158755037c0899b73d4ee
radha412 <100402853+radha412@users.noreply.github.com>
2023-09-20 15:04:09 +0530

    save changes

--- src/main/java/com/example/paypalpayment/controller/PaypalController.java
+++ src/main/java/com/example/paypalpayment/controller/PaypalController.java
@@ -3,10 +3,12 @@ package com.example.paypalpayment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.paypalpayment.entity.Order;
 import com.example.paypalpayment.service.PaypalService;
@@ -14,7 +16,8 @@ import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 
-@Controller
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class PaypalController {
 	
 	@Autowired
